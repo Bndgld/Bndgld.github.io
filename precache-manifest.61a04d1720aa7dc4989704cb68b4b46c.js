6114a8886bf259e270b63ab323fad085cc68cb5d
Bndgld <glad_adiwijaya@yahoo.co.id>
2019-11-21 15:38:02 +0700

    first commit

---
+++ precache-manifest.61a04d1720aa7dc4989704cb68b4b46c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9462190aa3527dfe8bd6",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "a1e26d93849ef10af2ca",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "afa05f3ce5712c6138b7070952cc7298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9462190aa3527dfe8bd6",
+    "url": "/js/about.8ef5c29d.js"
+  },
+  {
+    "revision": "6eccef974b93fdf759ab",
+    "url": "/js/app.2e757bb0.js"
+  },
+  {
+    "revision": "a1e26d93849ef10af2ca",
+    "url": "/js/chunk-vendors.cc45c6f9.js"
+  },
+  {
+    "revision": "b67b2e7920131b4377c5fc57133eeb0e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
